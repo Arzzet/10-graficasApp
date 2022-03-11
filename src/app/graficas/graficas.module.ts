5dfcc864f71160bc22556b1e9d4dc5ea0c3977eb
Iwo <iwoskater3@hotmail.com>
2022-03-11 09:33:10 +0100

    installed chartng and chartjs and created first bar chart example

--- src/app/graficas/graficas.module.ts
+++ src/app/graficas/graficas.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { NgChartsModule } from 'ng2-charts';
+
 import { GraficasRoutingModule } from './graficas-routing.module';
+
 import { BarrasComponent } from './pages/barras/barras.component';
 import { BarrasDoblesComponent } from './pages/barras-dobles/barras-dobles.component';
 import { DonaComponent } from './pages/dona/dona.component';
@@ -21,7 +24,8 @@ import { GraficasComponent } from './services/graficas/graficas.component';
   ],
   imports: [
     CommonModule,
-    GraficasRoutingModule
+    GraficasRoutingModule,
+    NgChartsModule
   ]
 })
 export class GraficasModule { }
