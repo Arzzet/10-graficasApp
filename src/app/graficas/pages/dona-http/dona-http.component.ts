b1949dc63b7e6ee92f80b02435aead7d80210313
Iwo <iwoskater3@hotmail.com>
2022-05-11 11:55:28 +0200

    finish and optimize doughnutHttp graph

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Color, ChartData, ChartType } from 'chart.js';
 import { GraficasService } from '../../services/graficas.service';
 
 @Component({
@@ -9,12 +10,56 @@ import { GraficasService } from '../../services/graficas.service';
 })
 export class DonaHttpComponent implements OnInit {
 
+  public colors: Color[] = [      
+    '#3366CC',
+    '#DC3912',
+    '#FF9900',
+    '#109618',
+    '#990099'
+  ];
+  
+  // Doughnut
+  public doughnutChartLabels: string[] = [ 
+    // 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' 
+  ];
+  public doughnutChartData: ChartData<'doughnut'> =
+    {
+      labels: this.doughnutChartLabels,
+      datasets: [
+        { 
+          data: []
+        },
+        
+      ]
+    }
+  
+  public doughnutChartType: ChartType = 'doughnut';
+
+
   constructor(private graficasservice: GraficasService) { }
 
   ngOnInit(): void {
-    this.graficasservice.getUsuariosRedesSociales()
-      .subscribe(resp => {
-        console.log(resp);
+    // this.graficasservice.getUsuariosRedesSociales()
+    //   .subscribe(data => {
+    //     console.log(data);
+    //     const labels = Object.keys (data);
+    //     const values = Object.values (data);
+
+        
+    //     this.doughnutChartData = {
+    //       labels: labels,
+    //       datasets: [{data: values, backgroundColor: this.colors}]
+    //     };
+
+        
+    //   });
+
+    this.graficasservice.getUsuariosRedesSocialesDonaData()
+      .subscribe( ({labels, values}) => {
+        this.doughnutChartData = {
+          labels,
+          datasets: [{data: values, backgroundColor: this.colors}]
+        };
       });
   }
 
